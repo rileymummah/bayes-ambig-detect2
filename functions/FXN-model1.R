d6bc44b2950f8e5f647bd5208dd0c14d102d59a9
Riley Mummah <rmummah@umass.edu>
2026-03-20 14:07:29 -0400

    Updated model1

--- functions/FXN-model1.R
+++ functions/FXN-model1.R
@@ -30,22 +30,19 @@ model1 <- function(i, input.data, param.combos, flag) {
     data <- list(y = x$y[,,2], # Detections only
                  nsites = x$nsites,
                  nindiv = x$nindiv,
-                 ntests = x$y[,,1] + x$y[,,2], # Non-detections + Detections
-                 delta = x$delta, r = x$r) # Assumed to be 0
+                 ntests = x$y[,,1] + x$y[,,2]) # Non-detections + Detections
   } else if(flag == 'ND') {
     # Make Us 0
     data <- list(y = x$y[,,2], # Detections only
                  nsites = x$nsites,
                  nindiv = x$nindiv,
-                 ntests = matrix(x$ntests, nrow = x$nsites, ncol = x$nindiv),
-                 delta = x$delta, r = x$r) # Assumed to be 0
+                 ntests = matrix(x$ntests, nrow = x$nsites, ncol = x$nindiv)) # Non-detections + Detections
   } else if (flag == 'D') {
     # Make Us 1
     data <- list(y = x$y[,,2] + x$y[,,3], # Detections and uncertain detections
                  nsites = x$nsites,
                  nindiv = x$nindiv,
-                 ntests = matrix(x$ntests, nrow = x$nsites, ncol = x$nindiv),
-                 delta = x$delta, r = x$r) # Assumed to be 0
+                 ntests = matrix(x$ntests, nrow = x$nsites, ncol = x$nindiv)) # Non-detections + Detections
   }
 
   # Initial values
@@ -60,14 +57,15 @@ model1 <- function(i, input.data, param.combos, flag) {
 
   # Initial conditions for latent state z, latent state w, and parameters
   inits <- function() {list(z = zinit,
-                            # w = winit,
                             psi = runif(1,0,1),
                             theta11 = runif(1,0,1),
-                            b1 = runif(1,x$delta,1))
+                            p111 = runif(1,0.5,1),
+                            p101 = runif(1,0,0.5),
+                            p001 = runif(1,0,0.5))
   }
 
   # Parameters to store
-  params <- c("psi", "theta11","b1")
+  params <- c("psi", "theta11","p111","p101","p001")
 
   print(paste0('Model 1-',flag,': Run ',i))
 
@@ -90,10 +88,12 @@ model1 <- function(i, input.data, param.combos, flag) {
   bind_cols(dataset = paste0('dataset',i),
             model = 'model1',
             flag = flag,
-            parameter = c('psi','theta11','b1','deviance'),
+            parameter = c('psi','theta11','p111','p101','p001','deviance'),
             true.mean = c(param.combos$psi[i],
                           param.combos$theta11[i],
-                          param.combos$b1[i],
+                          param.combos$p111[i],
+                          param.combos$p101[i],
+                          param.combos$p001[i],
                           NA),
             out$summary) %>%
     mutate(runtime = time,
