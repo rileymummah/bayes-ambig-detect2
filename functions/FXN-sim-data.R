cfd0af7a8387f073c2300955d898b6849261280a
Riley Mummah <rmummah@umass.edu>
2026-03-20 12:59:25 -0400

    Updated data README

--- functions/FXN-sim-data.R
+++ functions/FXN-sim-data.R
@@ -9,11 +9,10 @@
 #   ntests = number of samples processed per individual
 #   psi = true site occupancy
 #   theta11 = true site prevalence
-#   theta01 = individual contamination rate (assumed not to occur)
-#   p111 = true positive (sample-level) # CHANGE
-#   p100 = sample-level false positive # CHANGE
-#   p011 = sample- and site-level false positive # CHANGE
-#   p000 = site-level false positive (assumed not to occur) # CHANGE
+#   theta01 =  contamination rate (assumed not to occur)
+#   b1 = classification probability | pathogen at site & individual
+#   b2 = classification probability | pathogen at site but not individual
+#   b3 = classification probability | pathogen not at site & individual
 #   delta = diagnostic test sensitivity
 #   r = diagnostic test specificity
 #
@@ -29,7 +28,7 @@
 
 
 sim.data <- function(nsites, nindiv, ntests,
-                     psi, theta11, theta00, 
+                     psi, theta11, theta01, 
                      b1, b2, b3,
                      r, delta) {
   # Create dataset storage
@@ -50,7 +49,7 @@ sim.data <- function(nsites, nindiv, ntests,
   # Restrict w to 0 when z=0
   for (i in 1:nsites) {
     if (z[i] == 0) {
-      w[i,] <- rbinom(nindiv, 1, p = ((1-z[i])*(1-theta00))) # Always assigns 0s
+      w[i,] <- 0
     } else {
       w[i,] <- rbinom(nindiv, 1, p = (z[i]*theta11))
     }
@@ -64,20 +63,17 @@ sim.data <- function(nsites, nindiv, ntests,
       # P(Y=0)
       p[i,j,1] <- z[i]*w[i,j]*(1-p111) +
                   z[i]*(1-w[i,j])*(1-p101) + 
-                  # (1-z[i])*w[i,j]*(p011*(1-delta) + (1-p011)*(1-r)) + 
                   (1-z[i])*(1-w[i,j])*(1-p001)
       
       # P(Y=1)
-      p[i,j,2] <- z[i]*w[i,j]*(b1*p111) + 
-                  z[i]*(1-w[i,j])*(b2*p101) +
-                  # (1-z[i])*w[i,j]*(p011*(1-delta) + (1-p011)*r) + 
-                  (1-z[i])*(1-w[i,j])*(b3*p001)
+      p[i,j,2] <- z[i]*w[i,j]*b1*p111 + 
+                  z[i]*(1-w[i,j])*b2*p101 +
+                  (1-z[i])*(1-w[i,j])*b3*p001
       
       # P(Y=U)
-      p[i,j,3] <- z[i]*w[i,j]*((1-b1)*p111) +
-                  z[i]*(1-w[i,j])*((1-b2)*p101) + 
-                  # (1-z[i])*w[i,j]*(p011*delta + (1-p011)*(1-r)) + 
-                  (1-z[i])*(1-w[i,j])*((1-b3)*p001)
+      p[i,j,3] <- z[i]*w[i,j]*(1-b1)*p111 +
+                  z[i]*(1-w[i,j])*(1-b2)*p101 +
+                  (1-z[i])*(1-w[i,j])*(1-b3)*p001
       
       # Sample detection state from multinomial dist
       y[i,j,] <- rmultinom(1, ntests, prob=p[i,j,]) # [0, 1, U]
@@ -87,7 +83,7 @@ sim.data <- function(nsites, nindiv, ntests,
   
   return(list(z = z, w = w, y = y, p = p,
               nsites = nsites, nindiv = nindiv, ntests = ntests,
-              psi = psi, theta11 = theta11, theta00 = theta00, 
+              psi = psi, theta11 = theta11, theta01 = theta01, 
               b1 = b1, b2 = b2, b3 = b3,
               delta = delta, r = r))
 }
